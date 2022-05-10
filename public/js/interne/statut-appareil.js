6b8e29421c39c74be88662f76dc654420d601cf1
wasef <alex.wasef@gmail.com>
2022-05-10 11:29:29 +0200

    code color for statut & start to make more dynamic show client

--- public/js/interne/statut-appareil.js
+++ public/js/interne/statut-appareil.js
@@ -2,25 +2,32 @@ $(document).ready(function(){
     //console.log();
    // var statuts = document.querySelectorAll(".statutAppareil");
     var lignes = document.querySelectorAll(".ligne_client");
-    
+    //on récperer toute les lignes du tableau
     for(ligne of lignes){
-        //console.log(ligne.childNodes);
+        //on recuperer le statut de chaque ligne
         statut = ligne.childNodes
-
-        
-      
-        //statut.omeParentFindMethod('tr');
-       // console.log(statut);
+        // Pour chaque statut on lui attrbut une classe
+        console.log(statut[17].childNodes[1].innerText);
         if(statut[17].childNodes[1].innerText == 'Livré'){
-            console.log(statut[17].childNodes[1].innerText);
-            //statut[17].childNodes[1].classList.add('visible');
             ligne.classList.add('completed');
-            statut[17].childNodes[1].classList.add('bg-green');
-            //statut[17].classList.add('alert');
+            statut[17].childNodes[1].classList.add('bg-green-dark');
         }
-        else{
-            statut[17].childNodes[1].classList.add('hidden');
+        else if(statut[17].childNodes[1].innerText == 'Prêt à être récupéré') {
+            statut[17].childNodes[1].classList.add('bg-green-light');
         }
+        else if(statut[17].childNodes[1].innerText == 'Pris en charge'){
+            statut[17].childNodes[1].classList.add('bg-yellow-dark');
+        }
+        else if(statut[17].childNodes[1].innerText == 'En attente de pièce'){
+            statut[17].childNodes[1].classList.add('bg-purple-dark');
+        }
+        else if(statut[17].childNodes[1].innerText == 'Devis envoyée'){
+            statut[17].childNodes[1].classList.add('bg-blue-light');
+        }
+        else if(statut[17].childNodes[1].innerText == 'En cours de réparation'){
+            statut[17].childNodes[1].classList.add('bg-red-light');
+        }
+        
     }
 
  
