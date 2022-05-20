7f3c5968ce8caf054b60fcd960ddc1ff539cfd05
wasef <alex.wasef@gmail.com>
2022-05-20 17:52:56 +0200

    rdv dynamic

--- public/js/interne/completedRdv.js
+++ public/js/interne/completedRdv.js
@@ -9,18 +9,24 @@ function ajax_rdvCompleted(url,form,card,checkbox) {
     url: url,
     data: $(form).serialize(),
   }).done( function(response) { 
-    //var btnDelete = '<form method="post" action="/rdv/interne/'++'" onsubmit="return confirm(`Voulez-vous vraiment supprimer ce rendez-vous ?`);"> <input type="hidden" name="_token" value="5264386c0d1c099.bn2iqKbVeMI2lojlRfEqKOTRGEGqKLdHZciatNYtKfQ.Jx7xz9azF6pk2uaVAclIW5OgbHKaQfQFV_jNwr5LGpo7E8jJ6o098Wnd3Q"> <button class="btn btn-danger mr-2 "><img class="delete" src="/./images/icon-delete.png" width="20px"></button> </form>'
-    console.log(response,form);
-    console.log();  
-    
+    //var btnDelete = '<form method="post" action="/rdv/interne/'++'" onsubmit="return confirm(`Voulez-vous vraiment supprimer ce rendez-vous ?`);"> <input type="hidden" name="_token" value="5264386c0d1c099.bn2iqKbVeMI2lojlRfEqKOTRGEGqKLdHZciatNYtKfQ.Jx7xz9azF6pk2uaVAclIW5OgbHKaQfQFV_jNwr5LGpo7E8jJ6o098Wnd3Q"> <button class="btn btn-danger mr-2 "><img class="delete" src="/./images/icon-delete.png" width="20px"></button> </form>' 
+    var badgeDate = card.querySelector(".badge-date");
     if(checkbox.checked) {
       // va dans historique
-      card.querySelector(".badge-date").classList.add("hidden")
+      
+      if( badgeDate ){
+        badgeDate.classList.add("collapse")
+      }
+      card.querySelector(".button-hidden").classList.add("collapse")
+      
       // Si la case est cochée, la carte va dans historique
       $(".historyRdv").append('<div class="zoom">'+card.outerHTML+"</div>");
     }
     else if(!checkbox.checked) {
-      card.querySelector(".button-hidden").classList.remove("hidden")
+      if( badgeDate ){
+        badgeDate.classList.remove("collapse")
+      }
+      card.querySelector(".button-hidden").classList.remove("collapse")
       // Si la case n'est pas cochée, la carte va dans la liste d'appel
       $(".all-rdv").append('<div class="zoom">'+card.outerHTML+"</div>");
     }
@@ -28,12 +34,8 @@ function ajax_rdvCompleted(url,form,card,checkbox) {
     $(card).fadeOut( "slow", function() {
       $( card ).remove();
     });
-   
-   
-    
     reloadCardRdv()
     listenerCheckboxRdv()
-
     document.querySelector('.body').classList.remove("cursor-loader");
   }).fail(function(jxh,textmsg,errorThrown){
     console.log(textmsg);
