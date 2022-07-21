d58f2b1099ed53cccca54a370119404f8dcd9b40
Wasef81194 <alex.wasef@gmail.com>
2022-07-21 11:51:23 +0200

    new nav

--- public/js/interne/tache.js
+++ public/js/interne/tache.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var lignes = document.querySelectorAll('.ligne_tache');
+    var tables = document.querySelectorAll('.tableau-tache');
     for (ligne of lignes) {
         var thisLigne = ligne.childNodes;
         var doTache = thisLigne[9].lastElementChild.attributes.value.value; 
@@ -7,5 +8,12 @@ $(document).ready(function(){
             ligne.classList.add('completed');
         }
     }
+    for (table of tables) {
+        let tache = table.querySelector('.tache-faire').value
+        if(tache) {
+            table.classList.add('completed');
+        }
+    }
+
 
 })
